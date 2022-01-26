4d31186176e790e3f37f511f23b39da25c869ade
Olga Karpenko <okarpenko@usfca.edu>
2022-01-26 08:46:07 -0800

    lecture1 and lecture2 examples

---
+++ lecture1Basics/Hello.java
@@ -0,0 +1,12 @@
+public class Hello {
+
+	public static void main(String[] args) {
+
+		for (int i = 5; i > 0; i--) {
+			System.out.println(i);
+		}
+		
+		
+  	}
+
+}
---
+++ lecture2DataTypes/TempConverter.java
@@ -0,0 +1,25 @@
+
+// In class lab - TempConverter class
+// Based on the exam of Lewis/Loftus
+//
+//  Demonstrates the use of primitive data types and arithmetic
+//  expressions.
+//********************************************************************
+
+import java.util.Scanner;
+
+public class TempConverter
+{
+   //-----------------------------------------------------------------
+   //  Computes the Fahrenheit equivalent of a specific Celsius
+   //  value using the formula F = (9/5)C + 32.
+   //-----------------------------------------------------------------
+   public static void main (String[] args)
+   {
+      final int BASE = 32;
+      final double CONVERSION_FACTOR = 9.0 / 5.0;
+
+      // FILL IN CODE
+      
+   }
+}
