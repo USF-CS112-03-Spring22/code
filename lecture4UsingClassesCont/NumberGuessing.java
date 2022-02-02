e703e2719e0f0ed4755f5c9cbce7471304251149
Olga Karpenko <okarpenko@usfca.edu>
2022-02-02 00:18:08 -0800

    lecture 4 examples

---
+++ lecture4UsingClassesCont/NumberGuessing.java
@@ -0,0 +1,33 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class NumberGuessing {
+        
+        public static void guessNumber() {
+                Random rand = new Random();
+                int num = rand.nextInt(10);
+
+                System.out.println("Guess a number: ");
+                Scanner sc = new Scanner(System.in);
+                int userGuess = sc.nextInt();
+
+                if (num == userGuess)
+                        System.out.println("You guessed right");
+                else {
+                        System.out.println("Sorry, wrong guess");
+                        System.out.println("Guess a number again: ");
+                        userGuess = sc.nextInt();
+                        if (num == userGuess)
+                                System.out.println("You guessed right");
+                        else 
+                                System.out.println("Sorry, wrong guess");
+                        
+                }
+
+        }
+
+        public static void main(String[] args) {
+                guessNumber();
+                
+        }
+}
