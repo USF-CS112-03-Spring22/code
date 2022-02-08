eb1d3a0648322d21a93d433256af8c5a73c512d9
Olga Karpenko <okarpenko@usfca.edu>
2022-02-08 13:18:42 -0800

    Updated StringExample

--- lecture3UsingClasses/StringExample.java
+++ lecture3UsingClasses/StringExample.java
@@ -4,7 +4,7 @@ public class StringExample {
     public static void main(String[] args) {
 
         //declare a string reference
-        String s1;
+       /* String s1;
         // point it to an object
         s1 = new String("Luke Skywalker");
 
@@ -23,6 +23,10 @@ public class StringExample {
         char firstLetter = s1.charAt(0); // first letter of string s1
         System.out.println(firstLetter);
 
+        for (int i = 0; i < s1.length(); i++) {
+            System.out.println(s1.charAt(i));
+        }
+
         if (s2.endsWith("a"))
             System.out.println("String s2 ends with an a");
 
@@ -30,12 +34,20 @@ public class StringExample {
         if (s1.equals(s2)) {
             System.out.println("Yoda is the same as Luke Skywalker. Wait, what??");
         }
+
+        // s3.toLowercase(); // does not change s3
+        s3 = s3.toLowercase(); // will change s3
+        
+
         System.out.println(s3.toLowerCase());
         // note: s3 will not change! toLowercase method will return a new string in lowercase
 
         // Demonstrates concatenation using + operator
         String s = s2 + " is pretty cool, but so is " + s3;
-        System.out.println(s); 
+        System.out.println(s);  */
+
+        checkUsername();
+
 
 
     }
@@ -44,8 +56,69 @@ public class StringExample {
 
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter a username");
-        // FILL IN CODE
+        String str = sc.nextLine();
 
+        /*if (str.length() < 5) {
+            System.out.println("Too short");
+            return;
+        }
+        //if (str.charAt(0) != '-') {
+        if (!str.startsWith("-")) {
+             System.out.println("Does not start with a dash");
+             return;   
+        }
+
+        boolean foundA = false;
+
+        v
+
+        if (foundA) {
+            System.out.println("The username is fine.");
+        }
+        else
+            System.out.println("The username does not contain an a");
+         */   
+
+
+        // FILL IN CODE
+        if (!checkLength(str))
+            return;  
+
+        if (!checkFirstLetter(str, "-"))
+            return;  
+
+        if (!checkIfContainsLetter(str, 'a'))
+            return;
+
+        System.out.println("Username is ok");
+
+    }
+
+    public static boolean checkLength(String str) {
+        if (str.length() < 5) {
+            System.out.println("Too short");
+            return false;
+        }
+        return true;
+    }
+
+    public static boolean checkFirstLetter(String str, String characterWeWant) {
+        if (!str.startsWith(characterWeWant)) {
+             System.out.println("Does not start with the character we expect");
+             return false;   
+        }
+        return true;
+    }
+
+    public static boolean checkIfContainsLetter(String str, char letter) {
+
+        for (int i = 0; i < str.length(); i++) {
+            if (str.charAt(i) == letter) {
+                return true;
+            }
+        }
+        System.out.println("Did not find the given letter");
+        return false;
     }
         
 }
