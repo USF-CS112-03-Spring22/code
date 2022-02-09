2fce1fc069b445c1f64d8d32f37b5269e3e1f0ca
Olga Karpenko <okarpenko@usfca.edu>
2022-02-09 09:52:04 -0800

    Added lecture 6 examples

---
+++ lecture6Composition/Student.java
@@ -0,0 +1,12 @@
+public class Student {
+	private int id;
+	private String name;
+	private double score;
+
+	public Student(int id, String name, double score) {
+		this.id = id;
+		this.name = name;
+		this.score = score;
+	}
+
+}
---
+++ lecture6Composition/UserInterface.java
@@ -0,0 +1,47 @@
+import java.util.Scanner;
+
+public class UserInterface {
+	
+	private Calculator calc;
+
+	public UserInterface() {
+		calc = new Calculator();
+	}
+
+	public void interactWithUser() {
+		Scanner sc = new Scanner(System.in);
+		String op;
+		int num;
+		for (int i = 0; i < 5; i++) {
+			System.out.println("Enter operation: + or - or *");
+			op = sc.next();
+			System.out.println("Enter a number :");
+			num = sc.nextInt();
+			compute(op, num);
+		}
+
+	}
+
+	public void compute(String operation, int num) {
+		if (operation.equals("+"))
+			calc.add(num);
+		else
+			if (operation.equals("-"))
+				calc.substract(num);
+			else if (operation.equals("*"))
+				calc.multiply(num);
+		
+
+	}
+
+	public void printFinalResult() {
+		System.out.println("The final result is " + calc.getResult());
+	}
+
+	public static void main(String[] args) {
+		UserInterface ui = new UserInterface();
+		ui.interactWithUser();
+		ui.printFinalResult();
+
+	}
+}
