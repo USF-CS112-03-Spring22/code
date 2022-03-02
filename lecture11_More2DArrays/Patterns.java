5a023b3d187ff9e6dfef7bc259a622eceb3c8551
Olga Karpenko <okarpenko@usfca.edu>
2022-03-02 10:49:43 -0800

    Updated lecture 11 with some exercises

---
+++ lecture11_More2DArrays/Patterns.java
@@ -0,0 +1,30 @@
+public class Patterns {
+	
+
+	public static void printPattern(int n){
+		// FILL IN CODE
+		/* Prints the following pattern for n = 3 :
+		123
+		12
+		1
+	
+		Prints the following pattern for n = 5 :
+		12345
+		1234
+		123
+		12
+		1
+
+		Should work similarly for any n - make your code general
+		*/
+		
+		
+	}
+	
+	public static void main(String[] args) {
+		printPattern(6);
+	
+	}	
+
+
+}
